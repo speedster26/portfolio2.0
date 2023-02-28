fe395d608f4cce6cd5a55f6bc68f2f340b1183ba
Harshit Verma <harshitverma591@gmail.com>
2023-02-28 11:04:06 +0530

    About Image Fixed

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ const About = ({pageInfo}: Props) => {
         <h3 className='absolute flex top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             About
         </h3>
-        <motion.img initial={{x:-200,opacity:0}} whileInView={{x:0,opacity:1}} viewport={{once:true}} transition={{duration:1.2}} className="-mb-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:w-64 md:h-96" src={urlFor(pageInfo?.heroImage).url()} alt={"image"}/>
+        <motion.img initial={{x:-200,opacity:0}} whileInView={{x:0,opacity:1}} viewport={{once:true}} transition={{duration:1.2}} className="-mb-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:w-64 md:h-96" src={urlFor(pageInfo?.profilePic).url()} alt={"image"}/>
         <div className='md:space-y-10 space-y-5 px-0 md:px-10'>
             <h4 className='text-xl md:text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0B]/50'>little</span> background</h4>
             <p className='text-xs md:text-base'>
